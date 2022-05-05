07bbc8a76467943ab722439cd12d0d99d00bb706
Karol Zmarz <karol.zmarz2@gmail.com>
2022-05-05 14:03:43 +0200

    7.2 ImmutableUser

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,22 +1,11 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-
-        String poem = "Jeden pies";
-        String poem2 = "Dwa koty";
-        String poem3 = "Trzy chomiki";
-        String poem4 = "CZTERY JASZCZURKI";
-        String poem5 = "Piec koni";
-
-        poemBeautifier.beautify(poem, (p -> "ABC " + p + " ABC"));
-        poemBeautifier.beautify(poem2, String::toUpperCase);
-        poemBeautifier.beautify(poem3, (p -> p.replace('i', '1')));
-        poemBeautifier.beautify(poem4, String::toLowerCase);
-        poemBeautifier.beautify(poem5, (p -> p + " biegnie"));
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(100);
 
     }
 
