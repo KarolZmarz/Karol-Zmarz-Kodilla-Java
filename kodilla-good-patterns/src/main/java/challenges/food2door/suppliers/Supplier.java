210be4cb415b1ae0caf46a0df13f9637e64208e1
Karol Zmarz <karol.zmarz2@gmail.com>
2022-06-07 11:16:01 +0200

    13.4. Food 2 Door

--- kodilla-good-patterns/src/main/java/challenges/food2door/suppliers/Supplier.java
+++ kodilla-good-patterns/src/main/java/challenges/food2door/suppliers/Supplier.java
@@ -1,10 +1,9 @@
 package challenges.food2door.suppliers;
 
-import challenges.food2door.SupplyDTO;
 import challenges.food2door.Order;
+import challenges.food2door.SupplyDTO;
 
 public interface Supplier {
-
     String getName();
 
     SupplyDTO getSupplyInformation();
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/Main.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/Main.java
@@ -1,29 +1,36 @@
 package com.kodilla.good.patterns;
 
-import challenges.salesPortal.adressing.Client;
-import challenges.salesPortal.adressing.DeliveryDetails;
-import challenges.salesPortal.adressing.Order;
-import challenges.salesPortal.adressing.ProductOrderService;
-import challenges.salesPortal.adressing.cart.Cart;
-import challenges.salesPortal.adressing.cart.CartEntry;
-import challenges.salesPortal.adressing.products.Games;
-import challenges.salesPortal.adressing.products.Shoes;
+import challenges.food2door.AssistanceOrder;
+import challenges.food2door.Order;
+import challenges.food2door.Service.BasicConnectivityService;
+import challenges.food2door.Service.BasicOrderEngagingService;
+import challenges.food2door.Service.BasicStatusRecovery;
+import challenges.food2door.products.Carrot;
+import challenges.food2door.products.ProductDTO;
+import challenges.food2door.products.Tomato;
+import challenges.food2door.suppliers.FoodSupplier;
+import challenges.food2door.suppliers.HealthyShop;
+
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ProductOrderService productOrderService = new ProductOrderService();
-        Order order = generateOrder();
+        FoodSupplier supplier = new HealthyShop(new BasicConnectivityService(), new BasicStatusRecovery(), new BasicOrderEngagingService());
+        Order order = generateOrder(supplier);
+
+        System.out.println(supplier.getSupplyInformation());
+        long orderId = supplier.process(order);
+        System.out.println("Is order completed: " + supplier.isOrderComplete(orderId));
+        Order orderHealthyFood = AssistanceOrder.healthyShopOrderGenerate();
+        Order orderExtraFood = AssistanceOrder.extraFoodShopOrderGenerate();
+        Order orderGlutenFree = AssistanceOrder.GlutenFreeShopOrderGenerate();
     }
-    private static Order generateOrder() {
-        Client client = new Client("Karol");
-        Cart cart = new Cart();
-        cart.addCartEntry(new CartEntry(new Games("GTA V"), 1));
-        cart.addCartEntry(new CartEntry(new Shoes("Nike runner"), 2));
-
-        DeliveryDetails deliveryDetails = new DeliveryDetails("Krakow");
-
-        return new Order(client, cart, deliveryDetails);
+    private static Order generateOrder(FoodSupplier supplier) {
+        return new Order(supplier, List.of(
+                new ProductDTO(new Carrot(), 50),
+                new ProductDTO(new Tomato(), 100)
+        ));
     }
 }
