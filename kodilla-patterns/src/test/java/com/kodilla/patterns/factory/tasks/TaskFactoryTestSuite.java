9396da9647d2d429c4abb45608a4e101da358b56
Karol Zmarz <karol.zmarz2@gmail.com>
2022-06-19 20:07:31 +0200

    15.4 Factory

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/factory/tasks/TaskFactoryTestSuite.java
@@ -0,0 +1,52 @@
+package com.kodilla.patterns.factory.tasks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TaskFactoryTestSuite {
+
+    @Test
+    void testFactoryDriving() {
+        //Given
+        TaskFactory factory = new TaskFactory();
+        //When
+        Task driving = factory.makeTask(TaskFactory.DRIVING, "Driving", "garage", "car");
+        boolean beforeExecuted = driving.isTaskExecuted();
+        driving.executeTask();
+        boolean afterExecuted = driving.isTaskExecuted();
+        //Then
+        assertEquals("Driving", driving.getTaskName());
+        assertFalse(beforeExecuted);
+        assertTrue(afterExecuted);
+    }
+    @Test
+    void testFactoryPainting() {
+        //Given
+        TaskFactory factory = new TaskFactory();
+        //When
+        Task painting = factory.makeTask(TaskFactory.PAINTING, "Painting", "red", "strawberry");
+        boolean beforeExecuted = painting.isTaskExecuted();
+        painting.executeTask();
+        boolean afterExecuted = painting.isTaskExecuted();
+        //Then
+        assertEquals("Painting", painting.getTaskName());
+        assertFalse(beforeExecuted);
+        assertTrue(afterExecuted);
+    }
+    @Test
+    void testFactoryShopping() {
+        //Given
+        TaskFactory factory = new TaskFactory();
+        //When
+        Task shopping = factory.makeTask(TaskFactory.SHOPPING, "Shopping", "jacket", "1");
+        boolean beforeExecuted = shopping.isTaskExecuted();
+        shopping.executeTask();
+        boolean afterExecuted = shopping.isTaskExecuted();
+        //Then
+        assertEquals("Shopping", shopping.getTaskName());
+        assertFalse(beforeExecuted);
+        assertTrue(afterExecuted);
+    }
+
+}
